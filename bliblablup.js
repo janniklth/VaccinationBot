4f6ea411220e6f77b8c9cbc96afeca15ccd904f9
janniks mac <jannik.loth@gmx.de>
2021-06-20 23:00:55 +0200

    testing

---
+++ bliblablup.js
@@ -0,0 +1,7 @@
+class lol {
+    constructor(emojis, role_id, embed) {
+        this.emojis = emojis;
+        this.role_id = role_id;
+        this.embed = embed
+    }
+}
--- embed_messages.js
+++ embed_messages.js
@@ -1,8 +1,7 @@
 const { Client, MessageEmbed, DiscordAPIError, MessageReaction } = require('discord.js');
 const config = require('./configs/config');
 const fs = require('fs');
-const reactionRoles = require('./configs/reactionRoles');
-const reactionMessages = JSON.parse(fs.readFileSync('./data/reactionRoleMessages.json', 'utf-8'));
+
 
 // create embed
 var test_embed = new MessageEmbed()
@@ -11,4 +10,7 @@ var test_embed = new MessageEmbed()
     .addField("Übersicht \n", "1️⃣ ➔ BionTec/Pfizer \n 2️⃣ ➔ Moderna \n 3️⃣ ➔ AstraZeneca \n 4️⃣ ➔ Johnson & Johnson")
     .setColor('BLUE');
 
+
+
+// export all embeds
 exports.lol = test_embed;
